dab71472e2545627801b51f3289b68e8c4a9e9cf
Luke Blazewski <luke.blazewski@sofomo.com>
2021-11-21 23:56:37 +0100

    Fix powershell sshd_file_config and improve script logging

--- windows/configure_win.ps1
+++ windows/configure_win.ps1
@@ -10,12 +10,14 @@ $reg_file_name = "enableWol.reg"
 Invoke-WebRequest -Uri "${config_folder_uri}/${reg_file_name}" -OutFile $reg_file_name
 # Import the fil using reg programm
 reg import $reg_file_name
+Write-Output "Registry entries for WoL and startup modified successfully."
 
 # SSH server setup
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 # Start and enable the sshd service
 Start-Service sshd
 Set-Service -Name sshd -StartupType 'Automatic'
+Write-Output "SSH server installed successfully."
 # Confirm the Firewall rule is configured. It should be created automatically by setup. Run the following to verify
 if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
     Write-Output "Firewall Rule 'OpenSSH-Server-In-TCP' does not exist, creating it..."
@@ -29,10 +31,17 @@ $ssh_key_file_location = "C:\ProgramData\ssh\${ssh_key_file_name}"
 Invoke-WebRequest -Uri "${config_folder_uri}/${ssh_key_file_name}" -OutFile $ssh_key_file_location
 # Appropriately ACL the authorized_keys file on your server
 icacls.exe $ssh_key_file_location /inheritance:r /grant "Administrators:F" /grant "SYSTEM:F"
+Write-Output "Public key lab229 copied and access for administrators granted successfully for public keys file."
 # Disable password authentication and enable key auth
-$sshd_config_file = "C:\Windows\System32\OpenSSH\sshd_config_default"
+$sshd_config_file = "C:\ProgramData\ssh\sshd_config"
 (Get-Content -path $sshd_config_file -Raw) -match '^#PubkeyAuthentication yes' -replace '^PubkeyAuthentication yes'    | Set-Content -Path $sshd_config_file
 (Get-Content -path $sshd_config_file -Raw) -match '^#PasswordAuthentication yes' -replace '^PasswordAuthentication no' | Set-Content -Path $sshd_config_file
+Write-Output "SSH password authentication disabled and key auth enabled successfully."
 
 # ICMP firewall rule
-New-NetFirewallRule -Name 'ICMP Allow incoming V4 echo request' -DisplayName 'ICMP Allow (ping)' -Enabled True -Direction Inbound -Protocol ICMPv4 -IcmpType 8 -Action Allow
+if (!(Get-NetFirewallRule -Name "ICMP Allow incoming V4 echo request" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
+    Write-Output "Firewall Rule 'ICMP Allow incoming V4 echo request' does not exist, creating it..."
+    New-NetFirewallRule -Name 'ICMP Allow incoming V4 echo request' -DisplayName 'ICMP Allow (ping)' -Enabled True -Direction Inbound -Protocol ICMPv4 -IcmpType 8 -Action Allow
+} else {
+    Write-Output "ICMP Allow incoming V4 echo request' has been created and exists."
+}

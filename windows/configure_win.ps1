88d36c7ac2e7228f3d4e74910019cb4b8f7b04a2
Luke Blazewski <luke.blazewski@sofomo.com>
2021-11-22 14:28:44 +0100

    Disable error

--- windows/configure_win.ps1
+++ windows/configure_win.ps1
@@ -15,7 +15,7 @@ Remove-Item $reg_file_name
 
 # SSH server setup
 if ((Get-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0).State -eq "NotPresent") {
-    Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+    Add-WindowsCapability -ErrorActionSilentlyContinue -Online -Name OpenSSH.Server~~~~0.0.1.0
 }
 # Start and enable the sshd service
 Start-Service sshd

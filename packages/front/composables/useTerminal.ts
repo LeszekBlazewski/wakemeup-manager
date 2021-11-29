73f31f715f44d70bf8da387c9a6d936a4a7b9e54
damiankoper <kopernickk@gmail.com>
2021-11-29 21:34:38 +0100

    fix: permissions

--- packages/front/composables/useTerminal.ts
+++ packages/front/composables/useTerminal.ts
@@ -1,4 +1,4 @@
-import {  useRouter } from "@nuxtjs/composition-api";
+import { useRouter } from "@nuxtjs/composition-api";
 import { NodeState } from "~/../api/dist/types";
 import { useSnackbarStore } from "~/store/snackbarStore";
 
@@ -10,9 +10,14 @@ export function useTerminal() {
     return `clear; ssh ${state.username}@${state.host}`
   }
   async function runInTerminal(state: NodeState) {
-    await navigator.clipboard.writeText(getSshCommand(state))
-    router.push('/app/terminal?host='+state.host)
-    show('SSH command was copied to clipboard just in case!')
+    router.push('/app/terminal?host=' + state.host)
+
+    const queryOpts = { name: 'clipboard-write', allowWithoutGesture: false };
+    const result = await navigator.permissions.query(queryOpts as PermissionDescriptor);
+    if (result.state === 'granted') {
+      await navigator.clipboard.writeText(getSshCommand(state))
+      show('SSH command was copied to clipboard just in case!')
+    }
   }
   return { runInTerminal, getSshCommand }
 }
